1f3bbe1317a0ede05c7cbc824e505ca4dffc9b26
Franco Valerio <franco.gvp@hotmail.com>
2021-05-23 23:00:40 -0400

    feat: add signal and price alert webhooks for tradingview

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
-const { Composer } = require('micro-bot');
-const { getPrice } = require('./src/controllers/commands');
-const bot = new Composer;
+const { getPrice, cryptoBotCmd } = require('./src/controllers/commands');
 const app = require('./app.js');
+const { Telegraf } = require('telegraf');
+
+const token = process.env.BOT_TOKEN;
+if (token === undefined) {
+	throw new Error('BOT_TOKEN must be provided!');
+}
+
+const bot = new Telegraf(token);
 
 bot.command(['precio'], getPrice);
+
+bot.command(['cryptobot'], cryptoBotCmd);
 
 const port = process.env.PORT || 80;
 const now = new Date();
 
 app.listen(port, () => {
 	console.log(now + ' - Running server on port ' + port);
+	bot.launch();
+
+	// Enable graceful stop
+	process.once('SIGINT', () => bot.stop('SIGINT'));
+	process.once('SIGTERM', () => bot.stop('SIGTERM'));
 });
 
-module.exports = {
-	bot,
-};
+app.post('/webhook/alert', async (req, res) => {
+	const body = req.body;
+	bot.telegram.sendMessage(body.chatId, `${body.text}`, { parse_mode: body.parseMode })
+		.then(() => {
+			res.sendStatus(200);
+	});
+});
+
+module.exports = { bot };
