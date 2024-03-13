d30b3b582894bae548e17e193296f1e90b5e6932
Franco Valerio <fgvaleriop@falabella.cl>
2024-03-13 09:54:58 -0300

    refactor(webhooks/alert): improve debug logs

--- src/controllers/webhooks/handlers/alert/alert.js
+++ src/controllers/webhooks/handlers/alert/alert.js
@@ -8,14 +8,18 @@ function postAlert(bot) {
 		try {
 			let text = '';
 			if (typeof body === 'object' && 'text' in body) {
+                console.debug("webhook/alert handler: body is an object", text);
 				text = body.text;
 			} else {
+                console.debug("webhook/alert handler: body is text", text);
 				text = body;
 			}
-			if (bot != undefined) {
-				console.debug('Sending message to telegram chat ID' + chatId);
+			if (bot !== undefined) {
+				console.debug('Sending message to telegram chat ID: ' + chatId);
 				await bot.telegram.sendMessage(chatId, text, { parse_mode: 'MarkdownV2' });
-			}
+			} else {
+                console.debug("Bot is undefined");
+            }
 			res.sendStatus(200);
 		} catch (error) {
 			console.debug('webhook/alert handler: Error sending message to bot');
